971f1e1eef30ddde3a6e4cc7dfd3778d8ca78b38
ismail cadr <ismailcadir@gmail.com>
2019-01-17 01:05:45 +0300

    Mycontext ok

---
+++ MarketOtomasyonu/Market.Models/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using Market.Models.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Models.Entities
+{
+    class Category:BaseEntity<int>
+    {
+        [Required]
+        public string CName { get; set; }
+        [Required]
+        public decimal KDV { get; set; }
+       
+    }
+}
