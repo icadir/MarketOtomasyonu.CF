ae21de8723dae1457ae07397c1701c8908feb5bf
ismail cadr <ismailcadir@gmail.com>
2019-01-17 00:18:48 +0300

    Base Entityler ok

---
+++ MarketOtomasyonu/Market.Models/Abstracts/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Market.Models.Abstracts
+{
+    public class BaseEntity<T>
+    {
+        [Key]
+        [Column(Order = 1)]
+        public T Id { get; set; }
+    }
+}
